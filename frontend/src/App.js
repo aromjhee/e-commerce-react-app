a007dc147daabbb421c4c297ed93d37569500068
Arom Jhee <aromjhee@gmail.com>
2020-07-04 13:19:47 -0700

    move code to frontend directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open")
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>
+            &#9776;
+        </button>
+          <a href="index.html">imma zone</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="login.html">Log in</a>
+        </div>
       </header>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+        <ul>
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li>
+              <div className="product">
+                <img className="product-image" src="/images/d1.jpg" alt="product1" />
+                  <div className="product-name">
+                    <a href="product.html">Slim Shirt</a>
+                  </div>
+                  <div className="product-brand">Nike</div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        All rights reserved.
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
