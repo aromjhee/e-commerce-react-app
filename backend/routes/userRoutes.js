208bbf1ab1c72fc3d9847dd38a05c510a91ea0f0
Arom Jhee <aromjhee@gmail.com>
2020-07-06 10:01:19 -0700

    wip user creation

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import User from '../models/userModel';
+
+const router = express.Router();
+
+router.get('/createadmin', async (req, res) => {
+  try {
+    const user = new User({
+      name: 'admin',
+      email: 'admin@admin.com',
+      password: '1234',
+      isAdmin: true,
+    });
+  
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch(e) {
+    res.send({ msg: e.message });
+  }
+});
+
+router.post('/sign-in', async (req, res) => {
+  const { email, password } = req.body;
+
+  const logInUser = await User.findOne({
+    email,
+    password,
+  });
+
+  if (logInUser) {
+    const { id, name, email, isAdmin } = logInUser;
+    res.send({
+      id,
+      name,
+      email,
+      isAdmin,
+      token: getToken(user)
+    })
+  } else {
+    res.status(401).send({ msg: 'Invalid Email or Password' })
+  }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose'
 
+import config from './config';
 import { data } from './products';
+import userRoutes from './routes/userRoutes';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).catch(error => console.log(error.reason));
 
 const app = express();
+
+app.use('/api/users', userRoutes);
 
 app.get('/api/products/:id', (req, res) => {
   const productId = req.params.id;
