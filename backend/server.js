208b258004ab606cb9d4abb0d3554d09ebc355d9
Arom Jhee <aromjhee@gmail.com>
2020-07-08 14:58:44 -0700

    change path again

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
 })
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
