226017b676ab8e60b9e819b60b04e9ff93fd9e76
Arom Jhee <aromjhee@gmail.com>
2020-07-08 12:50:22 -0700

    change db url

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/imma-zone',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://heroku_jzp73kkn:kidq8cgn2t4k6c09cos3disdnr@ds113815.mlab.com:13815/heroku_jzp73kkn',
   JWT_SECRET: process.env.JWT_SECRET || 'secret'
 }
