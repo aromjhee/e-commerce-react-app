fce8dadabf1f96b1a4b8c295eca56ff5dd3a8e02
Arom Jhee <aromjhee@gmail.com>
2020-07-08 15:05:13 -0700

    wip

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const __dirname = path.resolve();
 
 app.use(bodyParser.json());
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
