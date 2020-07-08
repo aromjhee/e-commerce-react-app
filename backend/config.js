c6b1086b6559663dd03feb1f22b8ccb5958a2ad7
Arom Jhee <aromjhee@gmail.com>
2020-07-08 13:19:03 -0700

    wip

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export default {
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://heroku_jzp73kkn:kidq8cgn2t4k6c09cos3disdnr@ds113815.mlab.com:13815/heroku_jzp73kkn',
+  MONGODB_URL: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET || 'secret'
 }
